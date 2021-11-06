97d0dda35821455e810ccfc254cb78fe62dc56c9
takeshi <50539678+takeshi-furusawa@users.noreply.github.com>
2021-11-06 21:33:25 +0900

    fix: fzf .zsh path change

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/takeshi/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/takeshi/.fzf/bin"
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/takeshi/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/takeshi/.fzf/shell/key-bindings.zsh"
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
